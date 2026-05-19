9070fbd3308465e4c6430c9d2521501d6c2e33af
Hamza Maach <hamzamaach56@gmail.com>
2026-05-20 00:52:53 +0100

    fix: add the installing of npm and pm2 to the gateway script setup

--- scripts/gateway.sh
+++ scripts/gateway.sh
@@ -3,6 +3,16 @@ set -e
 
 source /vagrant/scripts/common.sh
 
+echo "Installing Node.js and PM2..."
+
+sudo apt-get update -y
+sudo apt-get install -y curl
+
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo npm install -g pm2
+
 echo "Setting up Gateway app..."
 cd /vagrant/srcs/api-gateway-app
 
