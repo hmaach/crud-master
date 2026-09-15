35f105d222eb0cc98893c461f4b86d51f438431f
alaidi <abdlekhaliklaidi@gmail.com>
2026-09-15 12:05:39 +0100

    Inventory, Billing API

--- srcs/billing-app/app/models.py
+++ srcs/billing-app/app/models.py
@@ -2,6 +2,7 @@ from .db import db
 
 
 class Order(db.Model):
+    #  __tablename__ = "orders"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(50))
     number_of_items = db.Column(db.Integer)
--- srcs/inventory-app/app/models.py
+++ srcs/inventory-app/app/models.py
@@ -2,6 +2,7 @@ from .db import db
 
 
 class Movie(db.Model):
+    # __tablename__ = "movies"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.Text)
